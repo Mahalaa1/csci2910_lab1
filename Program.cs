3e86ce85cfe5e4b92960fbd4e9e92a4921fa7283
Mahalaa1 <91162858+Mahalaa1@users.noreply.github.com>
2023-05-16 19:42:44 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csci2910_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
